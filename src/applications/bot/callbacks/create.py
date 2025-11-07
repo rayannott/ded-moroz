d56f0f234d95a0d70cbcd80a3ac2c9245eeecfd9
Airat Valiullin <airat.valiullin@entrixenergy.com>
2025-11-07 19:35:50 +0100

    changes

--- src/applications/bot/callbacks/create.py
+++ src/applications/bot/callbacks/create.py
@@ -1,8 +1,8 @@
 from loguru import logger
 from telebot import types
-
 from src.applications.bot.callbacks._base import Callback
 from src.models.user import User
+from telebot.formatting import escape_markdown
 from src.applications.bot.utils import get_keyboard, remove_keyboard
 
 
@@ -27,9 +27,12 @@ class CreateCallback(Callback):
 
     def _next_step(self, message: types.Message):
         option = text(message)
-        msg = f"{option!r} room creation selected. Code: `XXXX`"
+        # msg = rf"{option!r} room creation selected\. Code: {mcode('1234')}"
+        code = "1234"
+        msg = escape_markdown(f"Room creation selected: {option!r}. Code: `{code}`")
         self.bot.send_message(
             message.chat.id,
             msg,
+            parse_mode="MarkdownV2",
             reply_markup=remove_keyboard(),
         )
