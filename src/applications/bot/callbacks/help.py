ccc6361fe9cbabdb27dca42c443fe67086134edf
Airat <airat.valiullin@tum.de>
2025-11-10 00:43:38 +0100

    add "/" to help string

--- src/applications/bot/callbacks/help.py
+++ src/applications/bot/callbacks/help.py
@@ -7,14 +7,14 @@ from src.models.user import User
 
 HELP_MESSAGE = """ Play secret santa with your friends!
 Available commands:
-help - show this help message
-start - get started with the bot
-join - join a room
-name - set a display name
-me - show info about yourself
-create - create a new room
-manage - manage your rooms (start/delete games)
-leave - leave the current room
+/help - show this help message
+/start - get started with the bot
+/join - join a room
+/name - set a display name
+/me - show info about yourself
+/create - create a new room
+/manage - manage your rooms (start/delete games)
+/leave - leave the current room
 """
 
 
