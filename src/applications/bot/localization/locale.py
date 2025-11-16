5787ad9b6a56e761249e0303d5760859847872c3
Airat <airat.valiullin@tum.de>
2025-11-16 16:27:45 +0100

    add locale initial files

---
+++ src/applications/bot/localization/locale.py
@@ -0,0 +1,13 @@
+class Locale:
+    """A singleton class to manage localization settings.
+
+    Example usage:
+
+    >>> Locale("en")("start.new-user")
+    'Welcome, new user!'
+    >>> Locale("ru")("start.new-user")
+    'Добро пожаловать!'
+    """
+
+    # TODO(locale): implement actual localization loading and retrieval
+    pass
